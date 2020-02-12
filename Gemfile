425693fecb2e1e8581f61a7d1b161b2b72f9e207
Sonia Montero <soniamontero.dev@gmail.com>
2020-02-12 16:38:49 +0800

    ok

--- Gemfile
+++ Gemfile
@@ -24,6 +24,9 @@ gem 'omniauth-github'
 gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 gem 'pg_search', '~> 2.3.0'
 
+gem 'dotenv-rails', :require => 'dotenv/rails-now'
+
+
 group :development do
   gem 'web-console', '>= 3.3.0'
 end
--- app/helpers/background_colors_helper.rb
+++ app/helpers/background_colors_helper.rb
@@ -4,10 +4,8 @@ module BackgroundColorsHelper
       'login-background-color'
     elsif controller_name == 'experiences' && action_name == 'index'
       'light-green-background-color'
-    # elsif controller_name == 'experiences' && (action_name == 'new' || action_name == 'edit' || action_name == 'create' || action_name == 'update')
-    #   'light-blue-background-color'
-    # elsif controller_name == 'users' && action_name == 'edit'
-    #   'light-blue-background-color'
+    elsif controller_name == 'experiences' && action_name == 'show'
+      'dark-green-background-color'
     elsif controller_name == 'pages'
       'home-page-background-color'
     end
