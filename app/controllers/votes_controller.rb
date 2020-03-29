3a8788a58815b2de1771233f2f2a1700ad3a34cc
Sonia Montero <soniamontero.dev@gmail.com>
2020-03-29 17:39:24 +0800

    ajax vote btns

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,16 +6,22 @@ class VotesController < ApplicationController
     experience = Experience.find(params[:experience_id])
     @vote.experience = experience
     # TODO: AJAX
-    if @vote.save && params[:redirect_to] == "index"
-      redirect_to experiences_path
-    elsif @vote.save && params[:redirect_to] == 'show'
-      redirect_to experience_path(experience)
-    elsif @vote.save && params[:redirect_to].include?('profile')
-      username = params[:redirect_to].split[1]
-      redirect_to user_profile_path(username)
-    else
-      redirect_back(fallback_location: root_path)
+    if @vote.save
+      respond_to do |format|
+        format.html { redirect_to experiences_path }
+        format.js
+      end
     end
+    # if @vote.save && params[:redirect_to] == "index"
+    #   redirect_to experiences_path
+    # elsif @vote.save && params[:redirect_to] == 'show'
+    #   redirect_to experience_path(experience)
+    # elsif @vote.save && params[:redirect_to].include?('profile')
+    #   username = params[:redirect_to].split[1]
+    #   redirect_to user_profile_path(username)
+    # else
+    #   redirect_back(fallback_location: root_path)
+    # end
   end
 
   def update
@@ -26,28 +32,38 @@ class VotesController < ApplicationController
     elsif @vote.value == -1
       @vote.value = 1
     end
-    if @vote.save && params[:redirect_to] == 'show'
-      redirect_to experience_path(experience)
-    elsif @vote.save && params[:redirect_to] == "index"
-      redirect_to experiences_path
-    else
-      redirect_back(fallback_location: experiences_path)
+    if @vote.save
+      respond_to do |format|
+        format.html { redirect_to experiences_path }
+        format.js
+      end
     end
+    # if @vote.save && params[:redirect_to] == 'show'
+    #   redirect_to experience_path(experience)
+    # elsif @vote.save && params[:redirect_to] == "index"
+    #   redirect_to experiences_path
+    # else
+    #   redirect_back(fallback_location: experiences_path)
+    # end
   end
 
   def destroy
     @vote = Vote.find(params[:id])
     experience = @vote.experience
     @vote.destroy
-    if params[:redirect_to] == "index"
-      redirect_to experiences_path
-    elsif params[:redirect_to] == 'show'
-      redirect_to experience_path(experience)
-    elsif params[:redirect_to][0] == 'profile'
-      username = params[:redirect_to][1]
-      redirect_to user_profile_path(username)
-    else
-      redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      format.html { redirect_to experiences_path }
+      format.js
     end
+    # if params[:redirect_to] == "index"
+    #   redirect_to experiences_path
+    # elsif params[:redirect_to] == 'show'
+    #   redirect_to experience_path(experience)
+    # elsif params[:redirect_to][0] == 'profile'
+    #   username = params[:redirect_to][1]
+    #   redirect_to user_profile_path(username)
+    # else
+    #   redirect_back(fallback_location: root_path)
+    # end
   end
 end
